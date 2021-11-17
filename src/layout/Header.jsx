ce9b9db7f35c359bd11257bc384bf4256685ab5f
Uriel-Sj <uriel.soju@gmail.com>
2021-11-17 12:25:10 -0500

    cambios

---
+++ src/layout/Header.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom';
+
+import "./Header.styles.css"
+
+const Header = () => {
+
+    const [show, setShow] = useState(false)
+    const navigate = useNavigate();
+
+    // FUNCION CAMBIAR MENU 
+
+    const handlerMenu = ()=>{
+        setShow(!show)
+    }
+
+    return (
+        <>
+            <div className="header-container">
+                <a href="https://github.com/"> <i className="bi bi-github"></i></a>
+                <h2>Proyect Github</h2>
+                <button onClick={handlerMenu}><i className="bi bi-list colore"></i></button>
+
+            </div>
+
+            <div className={show ? "menuNav" : "notVisible"}>
+                <ul>
+                    <li onClick={()=> navigate('/')}>
+                      home
+                    </li>
+                    <li onClick={()=> navigate(- 1)}>
+                     previus
+                    </li>
+                    <li onClick={()=> navigate(+ 1)}>
+                     next
+                    </li>
+                </ul>
+        </div>
+      </>
+    )
+}
+
+export default Header
